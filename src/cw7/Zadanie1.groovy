271363eb8cc889d05b6cdaca3a66cbc4d0eea753
domino <domino2608@gmail.com>
2015-11-15 02:18:56 +0100

    Added cw7 zad1

---
+++ src/cw7/Zadanie1.groovy
@@ -0,0 +1,65 @@
+package cw7
+/**
+ * @author domino
+ * */
+
+/*
+ Firma software'owa prowadzi projekty w:
+ Groovy, Grails, Java, JEE
+ Plik Projekty.txt zawiera informacje o składzie osobowym każdego z projektów w postaci:
+ nazwaprojektu<TAB>Nazwisko imie(1)<TAB>Nazwiskoimie(2)<TAB> itd
+ Utworzyc pliki:
+ ProjektyDuze.txt - zawierający spis projektów w którym pracuje > niż 3 osoby
+ Programisci.txt - zawierajacy spis programistow w postaci:
+ nazwisko imie<tab>proj1<tab>proj2<tab> itd.
+ */
+
+file = new File("Projekty.txt")
+
+def projectMap = [:]
+def personProjectMap = [:]
+def bigProjectsList = []
+
+file.eachLine { line ->
+
+	def splittedLine = line.split("\t")
+
+	splittedLine[1..-1].each {
+		def list = projectMap[splittedLine[0]]
+
+		if(list == null)
+			projectMap[splittedLine[0]] = (list = [])
+		list << it
+
+		list = personProjectMap[it]
+
+		if(list == null)
+			personProjectMap[it] = (list = [])
+
+		list << splittedLine[0]
+	}
+}
+
+println projectMap
+println personProjectMap
+
+def largeProjectsFile = new File("ProjektyDuze.txt")
+
+projectMap.each {key, value ->
+	if(value.size() > 3){
+		largeProjectsFile.withPrintWriter { writer -> writer.println key }
+	}
+}
+
+def proggramersFile = new File("Programisci.txt")
+
+proggramersFile.withPrintWriter { writer ->
+	personProjectMap.each { key, value ->
+		writer.print(key + "\t")
+
+		value.each {
+			writer.print(it + "\t")
+		}
+		writer.println()
+	}
+}
